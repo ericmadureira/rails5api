9b42978a87378be60d51cb435d7dc9a4ba2c08ee
Eric Madureira <ericmadureira.uesc@gmail.com>
2018-10-10 10:47:38 -0300

    New endpoints and minor fixes

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -4,5 +4,39 @@ module Api
 			articles = Article.order('created_at DESC');
 			render json: {status: 'SUCCESS', message:'Articles loaded', data:articles}, status: :ok
 		end
+
+		def show
+			article = Article.find(params[:id])
+			render json: {status: 'SUCCESS', message:'Loaded article', data:article}, status: :ok
+		end
+
+		def create
+			article = Article.new(article_params)
+			if article.save
+				render json: {status: 'SUCCESS', message:'Saved article', data:article}, status: :ok
+			else
+				render json: {status: 'ERROR', message:'Articles not saved', data:article.errors}, status: :unprocessable_entity
+			end
+		end
+
+		def destroy
+			article = Article.find(params[:id])
+			article.destroy
+			render json: {status: 'SUCCESS', message:'Deleted article', data:article}, status: :ok
+		end
+
+		def update
+			article = Article.find(params[:id])
+			if article.update_attributes(article_params)
+				render json: {status: 'SUCCESS', message:'Updated article', data:article}, status: :ok
+			else
+				render json: {status: 'ERROR', message:'Articles not updated', data:article.errors}, status: :unprocessable_entity
+			end
+		end
+
+		private
+		def article_params
+			params.permit(:title, :body)
+		end
 	end
 end
